e0281b3f9af0c2be3e6f83d1f83d2bcaf233be3a
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-06-25 16:06:28 +0600

    explore event handle in react

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+
+import './App.css'
+
+function App() {
+  const clickHandling = () => {
+    alert ('Clicked Button');
+  }
+  
+  const addNumber = (num) => {
+    const newNumber = num + 5;
+    alert (newNumber);
+  }
+  return (
+    <>
+    <h1>React + Vite</h1>
+    {/* <button className='button' onclick = "clickHandling ()">Click Me</button> */}
+    <button className='button' onClick= {clickHandling}>Click Me</button>
+    <br/>
+    <button className='button' onClick= {function clickHandling () {
+      alert ('Now Click me!')
+    }}>Click Me 2</button>
+    <br />
+    <button className='button' onClick= {() => {
+      alert ('Clicked btn 3!')
+    }}>Clicked me 3</button>
+    <br />
+    <button className='button' onClick={() => addNumber(7)}>Clicked me 4</button>
+    </>
+          
+  )
+}
+
+export default App
